f3e30cee89aaa4b2151bf42d1ac04307dc1da7ee
Thiago Yonekura <104376017+thiagoyonekura@users.noreply.github.com>
2023-04-25 12:44:48 -0300

    Add JavaHerancaInterfacesPolimorfismo folder

---
+++ JavaHerancaInterfacesPolimorfismo/src/com/Heranca/Pessoa.java
@@ -0,0 +1,43 @@
+package com.Heranca;
+
+public class Pessoa {
+	private String nome;
+	private String edereco;
+	private String telefone;
+	private String cpf;
+	
+	private String telefoneCelular;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEdereco() {
+		return edereco;
+	}
+	public void setEdereco(String edereco) {
+		this.edereco = edereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefoneCelular() {
+		return telefoneCelular;
+	}
+	public void setTelefoneCelular(String telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+	
+}
---
+++ JavaHerancaInterfacesPolimorfismo/src/com/Heranca/Principal.java
@@ -0,0 +1,24 @@
+package com.Heranca;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		
+		Professor professor = new Professor();
+		
+		Pessoa pessoa = new Pessoa();
+		
+		aluno.setNome("Maria");
+		
+//		professor.setTelefoneCelular(telefoneCelular);
+		
+		Pessoa aluno2 = new Aluno();
+//		aluno.get
+		
+		Pessoa professor2 = new Professor();
+//		professor.get
+	}
+// Classe Pessoa também é chamada de superclasse ou classe pai/mãe
+}
